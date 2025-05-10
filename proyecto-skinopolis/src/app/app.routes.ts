965050dafa778594623d0415f5d7071f56cbdd76
sebastianimb <cirp8918@gmail.com>
2025-05-10 17:02:40 -0400

    Firts commit

--- proyecto-skinopolis/src/app/app.routes.ts
+++ proyecto-skinopolis/src/app/app.routes.ts
@@ -3,19 +3,24 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./products/home/home.component').then(m => m.HomeComponent)
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
     path: 'home',
-    loadComponent: () => import('./products/home/home.component').then(m => m.HomeComponent)
+    loadComponent: () =>
+      import('./products/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'productos',
-    loadChildren: () => import('./products/products.routes').then(m => m.PRODUCTS_ROUTES)
+    loadChildren: () =>
+      import('./products/products.routes').then((m) => m.PRODUCTS_ROUTES),
   },
   {
     path: '**',
-    loadComponent: () => import('./shared/components/error/error.component').then(m => m.ErrorComponent)
-  }
+    loadComponent: () =>
+      import('./shared/components/error/error.component').then(
+        (m) => m.ErrorComponent
+      ),
+  },
 ];
-
