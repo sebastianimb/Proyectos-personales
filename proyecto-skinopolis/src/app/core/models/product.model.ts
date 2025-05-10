e6d68e6f9cced9f6a034c261783b089d84cdda3e
sebastianimb <cirp8918@gmail.com>
2025-05-10 16:26:44 -0400

    Arreglo de maquetacion y colores

--- proyecto-skinopolis/src/app/core/models/product.model.ts
+++ proyecto-skinopolis/src/app/core/models/product.model.ts
@@ -6,6 +6,7 @@ export class Product {
     public gun: string,
     public exterior: 'FN' | 'MW' | 'FT' | 'WW',
     public float: number,
+    public rareLevel: string,
     public img: string,
     public pattern: number,
     public inspect: string,
