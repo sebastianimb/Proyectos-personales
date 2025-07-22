7429b61abc65052853eb0a1e299457ef85f85d4f
sebastian molina <cirp8918@gmail.com>
2025-07-22 19:37:04 -0400

    Update 01-variables.py

--- curso-django/02-fundamentos/01-variables.py
+++ curso-django/02-fundamentos/01-variables.py
@@ -1,8 +1,11 @@
+#variable string
 name = 'Sebastian'
+#las variables con mas palabras, por convención se escriben con guin bajo
 full_name = 'Sebastian Molina'
 
+#en python las variables y las constantes se escriben igual, tu logica dira si se toma como constante o variable
 PI= 3.1416
-
+#las variables privadas por convención se escriben con un guion bajo
 _private_name = 'Seba'
-
+#un print
 print(name, full_name)
