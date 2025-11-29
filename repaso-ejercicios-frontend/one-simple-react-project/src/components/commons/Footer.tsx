4a6efe2f25e2631d761202790601466d0ec13d22
sebastian molina <cirp8918@gmail.com>
2025-11-29 16:16:03 -0300

    fix: update copyright year in Footer component to 2025

--- repaso-ejercicios-frontend/one-simple-react-project/src/components/commons/Footer.tsx
+++ repaso-ejercicios-frontend/one-simple-react-project/src/components/commons/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 Sebastian Molina. Todos los derechos reservados.</p>
+      <p>&copy; 2025 Sebastian Molina. Todos los derechos reservados.</p>
     </footer>
   );
 }
